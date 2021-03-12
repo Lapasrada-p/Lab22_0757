4ad3d3944de0008581221d0e884dbae823f761c5
Lapasrada-p <76034405+Lapasrada-p@users.noreply.github.com>
2021-03-12 16:36:49 +0700

    completed

--- lab22_1.cpp
+++ lab22_1.cpp
@@ -64,69 +64,22 @@ void List::append(int d){
 }
 
 //Write List::remove() here
-#include<iostream>
-using namespace std;
+void List::remove(int num){
+	Node *q = new Node;
 
-
-class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
-};
-
-class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
-};
-
-Node::~Node(){
-    cout << data << " was deleted.\n";
-}
-
-void List::insert(int d,int idx){	
-	Node *n = new Node;
-	n->data = d;
-	
-	if(idx == 0){
-		n->next = root;
-		root = n;
-		return;
-	}
-	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
-		current = current->next;
-	}
-	n->next = current->next;
-	current->next = n;		
-}
-
-void List::show(){
-	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
-		current = current->next;
-		cout << current->data << " ";
-	}	
-}
-
-void List::append(int d){	
-	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
-	else{
+	if(num == 0){
+		q= root;
+		root =root->next;
+		delete q;
+		--size;
+	}else{
 		Node *current = root;
-		while(current->next){
+		for(int i = 0; i < num-1; ++i){
 			current = current->next;
 		}
-		current->next = n;
+		q = current->next;
+		current->next = current->next->next;
+		delete q;
+		--size;
 	}
-	size++;
 }
-
-//Write List::remove() here
